c2c84b0620e7dbbe61a678b4734eff54b4f5f591
JhafetGit <jonargj04@gmail.com>
2026-08-30 18:38:15 -0600

    inicia curso sobre html, css etc

---
+++ soluciones-curso/Proyectos/Startup/src/main.js
@@ -0,0 +1,11 @@
+import './css/normalize.css';
+import './css/variables.css';
+import './css/estilos.css';
+import './css/header.css';
+import './css/hero.css';
+import './css/caracteristicas.css';
+import './css/precios.css';
+import './css/footer.css';
+import './js/video.js';
+import './js/slideshow.js';
+import './js/caracteristicas.js';
